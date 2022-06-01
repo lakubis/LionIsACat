bd942ef4ea10a037a68b083d8220c12c3e4cbbc1
Leonardo Valentino Kosasih <77981342+leonv1602@users.noreply.github.com>
2022-06-01 13:23:58 +0700

    add movement and check station v1.0

--- MotorEV-code/agents.py
+++ MotorEV-code/agents.py
@@ -1,9 +1,46 @@
 #%%
+# Packages
+import numpy as np
+
+
+#%%
 #Ini adalah script yang diisi dengan agent
 
+class agents:
+    # Tiap agent memiliki 2 parameter sebagai berikut 
+    # R : jarak yang ditempuh dalam satu hari 
+    # battery_life : isi atau kapasitas dari baterai
+    def __init__(self, driving_r): 
+        self.battery_life = battery()
+        self.driving_r = driving_r
+        self.position_x = np.random.randint(1,100)
+        self.position_y = np.random.randint(1,100)
+    
+
+    def movement(self):
+        if check_station() == False :
+            '''random movement von neumann '''
+            explore() 
+        else : 
+            go_station()
+    
+    def go_station(): 
+        '''move to station no matter what'''
+        pass 
+
+    def explore(self): 
+        decision = np.random.randint(0,1)
+        if decision == 0 :
+            self.position_x = self.position_x + np.random.randint(-1,1) # can move left, right, or stay
+        else : 
+            self.position_y = self.position_y + np.random.randint(-1,1) # can move up, down, or stay
+    
+    def check_station(self): 
+        '''manhattan distance'''
+        
+        pass
+        
 # %%
-
-
 class baterai:
     num_of_batteries = 0
     degradation_rate = 0.00025 #Ini didapatkan dari standar baterai HP, secara umum setelah 800 charge cycle, battery health tinggal 80%/0.8
