eb25dd8272fcbd07c2b3adc8feaa3fb280870552
lakubis <cahyadi.felix@gmail.com>
2022-06-10 10:57:09 +0700

    Data collector

--- Felix_Learn/money_model.py
+++ Felix_Learn/money_model.py
@@ -6,6 +6,10 @@ from mesa.datacollection import DataCollector
 
 def compute_gini(model):
     agent_wealths = [agent.wealth for agent in model.schedule.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum(xi*(N-i) for i, xi in enumerate(x))/(N*sum(x))
+    return 1 + (1/N) - 2*B
 
 class MoneyAgent(Agent):
 
@@ -48,4 +52,5 @@ class MoneyModel(Model):
         self.datacollector = DataCollector(model_reporters={"Gini":compute_gini}, agent_reporters={"Wealth":"wealth"})
 
     def step(self):
+        self.datacollector.collect(self)
         self.schedule.step()
